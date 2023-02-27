6348e534544a1047b126b3516e614fa0ea3d4350
SteveBinary <SteveBinary@users.noreply.github.com>
2023-02-27 11:51:21 +0100

    Check for type safety.

--- src/logic/game.ts
+++ src/logic/game.ts
@@ -165,30 +165,30 @@ export function isPlayer(player: Field): boolean {
 
 export function checkPlayerWon(board: Field[], player: number): boolean {
     // horizontal
-    if (board[0] == player && board[1] == player && board[2] == player) {
+    if (board[0] === player && board[1] === player && board[2] === player) {
         return true
     }
-    if (board[3] == player && board[4] == player && board[5] == player) {
+    if (board[3] === player && board[4] === player && board[5] === player) {
         return true
     }
-    if (board[6] == player && board[7] == player && board[8] == player) {
+    if (board[6] === player && board[7] === player && board[8] === player) {
         return true
     }
     // diagonal
-    if (board[0] == player && board[4] == player && board[8] == player) {
+    if (board[0] === player && board[4] === player && board[8] === player) {
         return true
     }
-    if (board[2] == player && board[4] == player && board[6] == player) {
+    if (board[2] === player && board[4] === player && board[6] === player) {
         return true
     }
     // vertical
-    if (board[0] == player && board[3] == player && board[6] == player) {
+    if (board[0] === player && board[3] === player && board[6] === player) {
         return true
     }
-    if (board[1] == player && board[4] == player && board[7] == player) {
+    if (board[1] === player && board[4] === player && board[7] === player) {
         return true
     }
-    if (board[2] == player && board[5] == player && board[8] == player) {
+    if (board[2] === player && board[5] === player && board[8] === player) {
         return true
     }
 
