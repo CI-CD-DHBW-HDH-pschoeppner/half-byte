ba8792705a8c2535b452b1ff912eb63fd96e54a7
SteveBinary <SteveBinary@users.noreply.github.com>
2023-02-27 11:50:01 +0100

    Reformat files.

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,12 +1,12 @@
-import type { Field } from "../game";
+import type {Field} from "../game";
 import {randomMove, winningMove} from "./bot";
 
 // the easy bot:
 // - chooses the winning move, if it can win
 // - chooses a random move otherwise
 export function easyMove(board: Field[], own: Field): number {
-  const nextMoveWinning = winningMove(board, own);
-  if (nextMoveWinning !== -1) return nextMoveWinning;
+    const nextMoveWinning = winningMove(board, own);
+    if (nextMoveWinning !== -1) return nextMoveWinning;
 
-  return randomMove(board);
+    return randomMove(board);
 }
--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -7,27 +7,27 @@ import {randomMove, winningMove} from "./bot";
 // - chooses the middle (4) field, if it can
 // - chooses a random move otherwise
 export function mediumMove(board: Field[], own: Field): number {
-  const nextMoveWinning = winningMove(board, own);
-  if (nextMoveWinning !== -1) return nextMoveWinning;
+    const nextMoveWinning = winningMove(board, own);
+    if (nextMoveWinning !== -1) return nextMoveWinning;
 
-  const nextMovePetty = pettyMove(board, own);
-  if (nextMovePetty !== -1) return nextMovePetty;
+    const nextMovePetty = pettyMove(board, own);
+    if (nextMovePetty !== -1) return nextMovePetty;
 
-  if (board[4] === Field.EMPTY) return 4;
+    if (board[4] === Field.EMPTY) return 4;
 
-  return randomMove(board);
+    return randomMove(board);
 }
 
 // this bot:
 // - blocks the player from winning, if it can
 // - chooses a random move otherwise
 export function pettyMove(board: Field[], own: Field): number {
-  if (own === Field.EMPTY) return -1;
+    if (own === Field.EMPTY) return -1;
 
-  const otherPlayer = own === Field.PLAYER1 ? Field.PLAYER2 : Field.PLAYER1;
-  const otherMoveWinning = winningMove(board, otherPlayer);
+    const otherPlayer = own === Field.PLAYER1 ? Field.PLAYER2 : Field.PLAYER1;
+    const otherMoveWinning = winningMove(board, otherPlayer);
 
-  if (otherMoveWinning !== -1) return otherMoveWinning;
+    if (otherMoveWinning !== -1) return otherMoveWinning;
 
-  return randomMove(board);
+    return randomMove(board);
 }
